b0e7be9d7b4cd9c0df03db481d128703a0e981d5
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2019-02-12 13:26:33 +0900

    [add] userコマンド実装

--- src/command/command_factory.rb
+++ src/command/command_factory.rb
@@ -1,6 +1,7 @@
 require './src/command/impl/command_contest'
 require './src/command/impl/command_problem'
 require './src/command/impl/command_count'
+require './src/command/impl/command_user'
 
 class CommandFactory
   def self.get_instance(name, options)
