054b80ee89bd43864c4531a01cfcb188a28d42c3
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2019-02-12 13:54:23 +0900

    [add] CsvWriter実装

--- src/writer/csv_writer.rb
+++ src/writer/csv_writer.rb
@@ -1,5 +1,8 @@
 class CsvWriter
   def write(result)
-
+    return if result.length == 0
+    header = result[0].keys
+    puts header.join(',')
+    puts result.map{|r| header.map{|h| r[h]}.join(',')}
   end
 end
